5661dbbd50b17b33bcc23cc9811e18bb9183e188
plyus1313 <vsviktoriya13@gmail.com>
2018-05-11 14:52:18 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch(' https://whispering-river-36401.herokuapp.com/imageurl', {
+      fetch('https://shielded-refuge-54755.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -86,7 +86,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch(' https://whispering-river-36401.herokuapp.com/image', {
+          fetch('https://shielded-refuge-54755.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
